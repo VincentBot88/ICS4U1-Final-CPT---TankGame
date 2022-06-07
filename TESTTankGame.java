752e8db38e05db70e44246b094fd3f8f71c75a3f
VincentBot88 <vincent.cheng22@ycdsbk12.ca>
2022-06-07 09:59:26 -0400

    Test

---
+++ TESTTankGame.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class TESTTankGame implements ActionListener, KeyListener, MouseMotionListener{
+	//Properties
+	JFrame theFrame = new JFrame("Tanks");
+	TESTTankPanel thePanel = new TESTTankPanel();
+	Timer theTimer = new Timer(1000/60, this);
+	public JLabel resultLabel;
+	
+	//Methods
+	public void mouseMoved(MouseEvent evt){
+		thePanel.mouseX = evt.getX();
+		thePanel.mouseY = evt.getY();
+	}
+	public void mouseDragged(MouseEvent evt){
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent evt){
+		if(evt.getSource() == theTimer){
+			thePanel.repaint();
+		}	
+	}
+	
+	public void keyReleased(KeyEvent evt){
+		if(evt.getKeyChar() == 'w'){
+			thePanel.intP1DefY = 0;
+		}else if(evt.getKeyChar() == 's'){
+			thePanel.intP1DefY = 0;
+		}else if(evt.getKeyChar() == 'a'){
+			thePanel.intP1DefX = 0;
+		}else if(evt.getKeyChar() == 'd'){
+			thePanel.intP1DefX = 0;
+		}
+		if(evt.getKeyCode() == 32){
+			
+		}
+	}
+	public void keyPressed(KeyEvent evt){
+		if(evt.getKeyChar() == 'w'){
+			thePanel.intP1DefY = -2;
+		}else if(evt.getKeyChar() == 's'){
+			thePanel.intP1DefY = +2;
+		}else if(evt.getKeyChar() == 'a'){
+			thePanel.intP1DefX = -2;
+		}else if(evt.getKeyChar() == 'd'){
+			thePanel.intP1DefX = +2;
+		}
+		
+		if(evt.getKeyCode() == 32){
+			thePanel.bulletVelocity = 4;
+		}
+	}
+	public void keyTyped(KeyEvent evt){
+		
+	}
+	
+	//Constructor
+	public TESTTankGame(){
+		thePanel.setPreferredSize(new Dimension(1280, 720));
+		thePanel.addMouseMotionListener(this);
+		theFrame.setContentPane(thePanel);
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.pack();
+		theFrame.setVisible(true);
+		theFrame.setResizable(false);
+		theFrame.addKeyListener(this);
+		theTimer.start();
+	}
+
+	//main method
+	public static void main(String[] args){
+		new TESTTankGame();
+	}
+
+}
---
+++ TESTTankPanel.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class TESTTankPanel extends JPanel{
+	//Properties
+	int intP1Y = 100;
+	int intP1X = 100;
+	int intP1DefX = 0;
+	int intP1DefY = 0;
+	int mouseX;
+	int mouseY;
+	double bulletX = intP1X;
+	double bulletY = intP1Y;
+	boolean fire = false;
+	double bulletVelocity = 0; //however fast you want your bullet to travel
+	
+	//Methods
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		//Background
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 1280, 720);
+		//P1
+		g.setColor(Color.BLACK);
+		g.fillRect(intP1X, intP1Y, 30, 30);
+		// movement
+		intP1Y = intP1Y + intP1DefY;
+		intP1X = intP1X + intP1DefX;
+		
+		//bullet
+		
+        //mouseX/Y = current x/y location of the mouse
+        //originX/Y = x/y location of where the bullet is being shot from
+        double angle =(Math.atan2(mouseX - intP1X, mouseY - intP1Y));
+        double xVelocity = ((bulletVelocity) * Math.cos(angle));
+        double yVelocity = ((bulletVelocity) * Math.sin(angle));
+		g.fillRect((int)bulletY, (int)bulletX, 10, 10);
+		bulletX = bulletX + xVelocity;
+		bulletY = bulletY + yVelocity;
+		if(bulletX > 1280 || bulletY > 720){
+			bulletVelocity = 0;
+			bulletX = intP1Y;
+			bulletY = intP1X;
+		}
+	}
+	
+	//Constructor
+	public TESTTankPanel(){
+		super();
+	}
+
+
+
+}
