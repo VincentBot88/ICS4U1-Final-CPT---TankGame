09139f032cf37d1bc1f8f490f82efbd9a3f59986
elill2004 <elillmath2004@gmail.com>
2022-06-13 09:53:39 -0400

    Create servermodetank.java

---
+++ servermodetank.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.net.*;
+
+public class servermodetank implements ActionListener{
+
+
+
+
+
+
+
+
+
+
+}
